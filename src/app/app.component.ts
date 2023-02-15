6be71b839978e99403ba399bf4bca11803810094
MathisPastreNelson <shiray@hotmail.fr>
2023-02-15 16:11:29 +0100

    Initialisation du projet création du model et des données

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+// Importation des datas
+import { POKEMONS } from "./mock-pokemon-list";
+// Importation du model
+import { Pokemon } from "./pokemon";
 
 @Component({
   selector: "app-root",
-  template: ` <h1>Welcome to {{ title }}!</h1> `,
+  template: ` <h1>Liste de pokémons</h1> `,
 })
-export class AppComponent {
-  title = "angular-training-app";
+export class AppComponent implements OnInit {
+  pokemonList: Pokemon[] = POKEMONS;
+
+  ngOnInit() {
+    console.table(this.pokemonList);
+    this.selectPokemon(this.pokemonList[3]);
+  }
+
+  selectPokemon(pokemon: Pokemon) {
+    console.log(`Vous avez cliqué sur le ${pokemon.name}`);
+  }
 }
